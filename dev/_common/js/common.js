aa1c622de111948d83b3a15eb0deedb060359e30
Gar <gar@lonelydatum.com>
2024-04-01 15:57:11 -0400

    on

--- dev/_common/js/common.js
+++ dev/_common/js/common.js
@@ -1,6 +1,7 @@
 import {olg} from "./proline"
+import {origin} from "./helpers/helpers.js"
 const banner = document.getElementById('banner')
-const size = {w:banner.offsetWidth, h:banner.offsetHeight}
+const bannerSize = {w:banner.offsetWidth, h:banner.offsetHeight}
 
 gsap.defaults({
   ease: "power3.out"
@@ -8,20 +9,57 @@ gsap.defaults({
 
 
 
-const READ = {
-	t1: 2.5,
-	t2a: 2.8,
-	t2b: 2.3,
+const READ_T1 = {}
+const READ_T2 = {}
+const READ_T3 = {}
+
+READ_T1["nba-finals"] = 2.2
+READ_T2["nba-finals"] = 1.8
+READ_T3["nba-finals"] = 2
+
+READ_T1["nba-playin"] = 1.4
+READ_T2["nba-playin"] = 1.8
+READ_T3["nba-playin"] = 2
+
+READ_T1["nba-playoffs"] = 2
+READ_T2["nba-playoffs"] = 1.8
+READ_T3["nba-playoffs"] = 2
+
+READ_T1["nhl"] = 2
+READ_T2["nhl"] = 2.5
+READ_T3["nhl"] = 2
+
+
+const {w, h} = bannerSize
+
+function logoFader(){
+	const tl = new TimelineMax()
+	tl.to(".logo1", {duration:.2, opacity:0}, "+=.5")
+	return tl
+}
+
+function bgFadeOut(read){
+	const tl = new TimelineMax()
+	tl.to([ ".bg", ".t1"], {duration:.2, opacity:0}, `+=${read}`)
+	return tl
+}
+
+function fader(el, time){
+	const tl = new TimelineMax()
+	tl.from(el, {duration:.3, opacity:0}, "+=.2")
+	tl.to(el, {duration:.3, opacity:0}, `+=${time}`)
+	return tl
+}
+
+function ender(){
+	const tl = new TimelineMax()
+	// tl.from([".t3", ".logo3"], {duration:.3, opacity:0}, "+=.2")
+	tl.from([".footer"], {duration:.3, opacity:0}, "+=.5")
+	tl.add(olg())
+	return tl	
 }
 
 
-
-const {w, h} = size
-
-
-
-
-	
 function init(){	
 	const tl = new TimelineMax({onComplete:()=>{
 		if(document.getElementById("legalBtn")){			
@@ -29,78 +67,112 @@ function init(){
 		}
 	}})
 	tl.set(".frame1", {opacity:1})
+	if(window.universalBanner.name==="nhl"){		
+		document.getElementById("legalContent").innerHTML = "Individuals must be 19 years of age or older, a resident of Ontario, and located in the province to participate in online PROLINE+ sports betting. Terms and Conditions Apply.<br/>NHL, the NHL Shield and the word mark and image of the Stanley Cup are registered trademarks and the Stanley Cup Playoffs logo is a trademark of the National Hockey League. © NHL 2024. All Rights Reserved."
+	}
 	return tl
 }
 
 
-let data_ = {}
-const ease = "power2.out"
 
-function pan(tl, a, b, delay){
-	
-	const TIME_XXX = w/300
-	let TIME = Math.min(TIME_XXX*.7, 1.1)
-	// TIME = Math.max(TIME, 1.3)
 
+function slider(read=2){	
+	const tl = new TimelineMax()
+	tl.add(logoFader())
+
+	tl.add("t1")
+	tl.from(".t1a", {duration:.26, x:"-=130", y:"+=30", opacity:0}, "t1")
+	tl.from(".t1b", {duration:.26, x:"+=130", y:"-=30", opacity:0}, "t1+=.4")
 	
-	tl.set(b, {opacity:1})
-	tl.add(a, delay)
-	tl.to(a, {duration:TIME, x:-w, ease:"power4.out"}, a)
-	tl.from(b, {duration:TIME, x:w, ease:"power4.out"}, a)
+	tl.add(bgFadeOut(READ_T1[universalBanner.name]))
+
+	return tl
 }
 
+console.log(READ_T2[universalBanner.name]);
+console.log(READ_T2);
 
-function start(data){
+function standard(){	
+	const tl = init()	
+	tl.add(slider(), "+=.5")
 	
-	// const read
-	
-	const tl = init()
-	const TIME_XXX = h/250
-	let TIME = Math.min(TIME_XXX*2, 2.8)
-	TIME = Math.max(TIME, 1.3)
-	
-	const F1_Y = -250
-	
-	tl.add("lax-1")
-	tl.to(".f1-bg_1", {duration:TIME*1.2, y:0}, "lax-1")
-	tl.from(".f1-balloon-1", {y:F1_Y, duration:TIME*.8, ease:ease}, "lax-1")
-	tl.from(".f1-balloon-2", {y:F1_Y, duration:TIME*.9,  ease:ease}, "lax-1")
-	tl.from(".f1-balloon-3", {y:F1_Y, duration:TIME*1,  ease:ease}, "lax-1")
-	tl.from(".f1-txt", {y:F1_Y, duration:TIME*1.1,  ease:"back.out"}, "lax-1")
-	tl.from(".f1-max", {y:F1_Y, duration:TIME*1.2,  ease:ease}, "lax-1")
+	tl.from(".t2", {duration:.3, opacity:0}, "+=.3")
+	tl.to(".t2", {duration:.2, opacity:0}, `+=${READ_T2[universalBanner.name]}`)
 
-	pan(tl, ".frame1", '.frame2', "+=.5")
+	tl.from(".t3", {duration:.3, opacity:0}, "+=.3")
+	tl.to(".t3", {duration:.2, opacity:0}, `+=${READ_T3[universalBanner.name]}`)
 	
+	tl.from(".t4", {duration:.3, opacity:0}, "+=.3")
+	tl.from(".cta", {duration:.3, opacity:0}, "+=.3")
 
-	tl.from(".f2-txta", {duration:.3, x:-100, y:"+=20", opacity:0, ease:"back.out"})
-	tl.from(".f2-txtb", {duration:.3, x:-100, y:"+=20", opacity:0, ease:"back.out"})
 	
+	tl.from([".footer"], {duration:.3, opacity:0}, "+=.5")
+	tl.add(olg(), "-=.3")
 
-	pan(tl, ".frame2", '.frame3', "+=2")
+	return tl
+}
 
-	tl.from(".f3-txta", {duration:.5, y:"+=20", x:-100, opacity:0, ease:"back.out"}, "-=.3")
-	tl.from(".f3-txtb", {duration:.5, opacity:0, ease:"back.out"})
-	
+function b_970x250(){
+	b_728x90()
+}
 
-	pan(tl, ".frame3", '.frame4', "+=2.3")
+function b_160x600(){
+	standard()
+}
 
-	tl.from(".f4-o", {duration:.5, scale:0,  rotate:70, ease:"back.out"})
-	
-	tl.add("end", "+=.3")	
-	tl.from(".f4-cta", {duration:.3, opacity:0, ease:"back.out"}, "end")
-	tl.add(olg(), "end")
+function b_300x250(){		
+	standard()
+}
 
-	// tl.play(".frame3")
+function b_300x600(){		
+	standard()
+}
+
+function b_1000x700(){	
+	standard()
+}
+
+function b_970x70(){
 
 }
 
+function b_320x50(){
+	
+
+	const tl = init()	
+	
+	tl.add("t1")
+	
+
+	tl.to(".logo1", {duration:.2, opacity:0}, "+=.5")
+
+	tl.add("t1")
+	tl.from(".t1a", {duration:.2, y:"-=50", opacity:0}, "t1")
+	tl.from(".t1b", {duration:.2, y:"+=50", opacity:0}, "t1+=.5")
+	
+	
+
+	tl.to([ ".bg", ".t1"], {duration:.2, opacity:0}, `+=${READ_T1[universalBanner.name]}`)
 
 
+	tl.from(".t2", {duration:.3, opacity:0}, "+=.3")
+	tl.to(".t2", {duration:.2, opacity:0}, `+=${READ_T2[universalBanner.name]}`)
 
+	tl.from(".t3", {duration:.3, opacity:0}, "+=.3")
+	tl.to(".t3", {duration:.2, opacity:0}, `+=${READ_T3[universalBanner.name]}`)
+	
+	tl.from(".t4", {duration:.3, opacity:0}, "+=.3")
+	tl.from(".cta", {duration:.3, opacity:0}, "+=.3")
 
+	
+	tl.from([".footer"], {duration:.3, opacity:0}, "+=.5")
+	tl.add(olg(), "-=.3")
 
+	return tl
+}
 
-export {size, init, start}
+function b_728x90(text1){
+	
+}
 
-
-
+export { init, b_160x600, b_300x250, b_300x600, b_320x50, b_728x90, b_970x250, b_1000x700,b_970x70, origin, standard,  slider, ender, logoFader, bgFadeOut }
--- dev/_common/js/proline.js
+++ dev/_common/js/proline.js
@@ -2,7 +2,7 @@
 CustomEase.create("custom", "M0,0 C0.14,0 0.234,0.438 0.264,0.561 0.305,0.728 0.4,1.172 0.55,1.172 0.652,1.172 0.722,1.102 0.77,1.024 0.834,0.93 0.89,0.946 0.916,0.946 0.952,0.946 1,1 1,1 ")
 
 function olg(){
-    TweenLite.set("#olg", {opacity:1})  
+	TweenLite.set("#olg", {opacity:1})	
     const tl = new TimelineMax()
         
     tl.to("#bluewedge1", {duration:.5, ease: 'power1.inOut', scaleY:1, scale:1, x:0, y:0}, 0)
